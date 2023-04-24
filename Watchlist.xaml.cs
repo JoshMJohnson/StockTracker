6c289239a421aee03f6bb39cfc4fae4b412fc76f
JoshMJohnson <joshjohns21@gmail.com>
2023-04-24 13:21:43 -0500

    Update Watchlist.xaml.cs

--- Watchlist.xaml.cs
+++ Watchlist.xaml.cs
@@ -9,11 +9,11 @@ public partial class Watchlist : ContentPage
 
     private async void Add_Stock(object sender, EventArgs e)
     {
-        string stock = await DisplayPromptAsync("Add Stock", "Enter stock ticker", "Add", keyboard: Keyboard.Text);
+        string stock = await DisplayPromptAsync("Add Stock", "Enter stock ticker", "Add", keyboard: Keyboard.Text, maxLength: 5);
     }
 
     private async void Remove_Stock(object sender, EventArgs e)
     {
-        string stock = await DisplayPromptAsync("Remove Stock", "Enter stock ticker", "Remove", keyboard: Keyboard.Text);
+        string stock = await DisplayPromptAsync("Remove Stock", "Enter stock ticker", "Remove", keyboard: Keyboard.Text, maxLength: 5);
     }
 }
