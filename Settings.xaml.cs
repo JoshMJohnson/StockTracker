15f73ac4eaf585a28063cbfde1713def6ac42e4d
JoshMJohnson <joshjohns21@gmail.com>
2023-04-26 00:17:05 -0500

    Redesigned settings inputs

--- Settings.xaml.cs
+++ Settings.xaml.cs
@@ -1,7 +1,19 @@
+using System.Numerics;
+
 namespace StockTracker;
 
 public partial class Settings : ContentPage
 {
+    /* app setting variables */
+    
+    /*
+    public static string notification_toggle = "On";
+    public static double percent_change = 5.0;
+    public static int notify_type = 0;
+    public static string tod1 = "10 am";
+    public static string tod2 = "2 pm";
+    */
+
 	public Settings()
 	{
 		InitializeComponent();
@@ -11,6 +23,19 @@ public partial class Settings : ContentPage
     private async void Notification_Toggle(object sender, EventArgs e)
     {
         await DisplayAlert("Pop up", "Notification_Toggle", "OK");
+
+        /* 
+         * if (notification_toggle == "On") notification_toggle = "Off";
+         * else notification_toggle = "On";
+         */
+
+
+    }
+
+    /* handles action when percent change threshold for push notification is changed */
+    private async void Percent_Notify_Change(object sender, EventArgs e)
+    {
+        await DisplayAlert("Pop up", "Percent_Notify_Change", "OK");
     }
 
     /* changes value of the percent change a stock has to make for a push notification */
@@ -23,7 +48,7 @@ public partial class Settings : ContentPage
      *  - option 1: send notifications at a given time (ex: 10am CST) 
      *  - option 2: send notifications at the exact time the stock reaches the change percentage (if overnight then send on open) */
     private async void Notify_Type_Change(object sender, EventArgs e)
-    {
+    { 
         await DisplayAlert("Pop up", "Notify_Type_Change", "OK");
     }
 
---
+++ Stock.cs
@@ -0,0 +1,7 @@
+namespace StockTracker;
+
+public class Stock : ContentPage
+{
+	public string ticker_name { get; set; }
+	public string ticker_price { get; set; }
+}
