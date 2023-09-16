fba5a5a8db394f5aea74a6bfd99d798ea5669ec6
Josh Johnson <107897805+JoshMJohnson@users.noreply.github.com>
2023-09-15 23:24:18 -0500

    remove personal api key value

--- StockRepository.cs
+++ StockRepository.cs
@@ -43,7 +43,7 @@ public class StockRepository
             await Init_Database();
 
             /* call api to retrieve stock ticker data */
-            string api_key = "1a5736c710a640679295533e0c6a53ca"; /* each user of the app should have a unique api key to stay under the GET Request limits */
+            string api_key = ""; /* TODO each user of the app should have a unique api key to stay under the GET Request limits */
             string download_url = $"https://api.twelvedata.com/quote?symbol={stock_ticker}&apikey={api_key}";
 
             WebClient wc = new WebClient();
